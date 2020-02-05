058a1f0709af460de011eda907231be2de0b11b4
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-02-05 18:40:53 +0100

    FIX Country route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
 import Countries from './components/views/Countries/CountriesContainer';
 import Regions from './components/views/Regions/RegionsContainer';
+import Country from './components/views/Country/CountryContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -45,6 +46,7 @@ class App extends React.Component {
             {/* TODO - add more routes for other views */}
             <Route exact path='/countries' component={Countries} />
             <Route exact path='/regions' component={Regions} />
+            <Route exact path='/country/:id' component={Country} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
